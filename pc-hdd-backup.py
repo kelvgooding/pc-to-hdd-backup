15fadec44e0cab87447c9a30152e037fe4556245
Kelv Gooding <82043281+kelvgooding@users.noreply.github.com>
2023-03-27 12:55:54 +0100

    Add files via upload
    Removed multiple functions have been reducded to one. Parameters added into the funciton to allow for multiple options.
    A timer has been added to show how long each section has taken in seconds once backup is complete.

--- pc-hdd-backup.py
+++ pc-hdd-backup.py
@@ -1,149 +1,74 @@
+"""
+Author: Kelvin Gooding
+Created: 2022-06-13
+Updated: 2023-03-27
+Version: 1.2
+"""
+
 # Modules
 
 import shutil
-import datetime
+from datetime import datetime
 import os
+import time
 
 # Variables
 
 user = os.getlogin()
+timestamp = datetime.today().strftime("%H:%M:%S")
 
-# Source
+# Script
 
-pc_documents = fr"C:\Users\{user}\Dropbox"
-pc_pictures = r"D:\Pictures"
-pc_music = r"D:\Music"
-pc_videos = r"D:\Videos"
-pc_software = r"D:\Software"
+input("Press any key to start ..")
 
-# Destination
 
-hdd_documents = r"E:\Documents"
-hdd_pictures = r"E:\Pictures"
-hdd_music = r"E:\Music"
-hdd_videos = r"E:\Videos"
-hdd_software = r"E:\Software"
+# Write to log file with Backup Start Time
 
-# Copy from PC to HDD
+def file_bkup(bkup_source, bkup_destination):
+    start_time = time.time()
 
-input("Press any key to start PC to HDD backup ..")
-print("\n{} - Backup Started!".format(datetime.datetime.today().strftime("%H:%M:%S")))
+    if os.path.isdir(bkup_destination):
+        print(f'\nExisting Directory Found! Deleting {bkup_destination} ..')
+        os.system(f'rmdir /S /Q "{bkup_destination}"')
 
-try:
+        print(f'{datetime.today().strftime("%H:%M:%S")} - Copying {bkup_source} to {bkup_destination}')
+        shutil.copytree(bkup_source, bkup_destination)
 
-    # Documents
+        print(f'{datetime.today().strftime("%H:%M:%S")} - Complete!')
+    else:
+        print(f'\n{datetime.today().strftime("%H:%M:%S")} - Copying {bkup_source} to {bkup_destination}')
+        shutil.copytree(bkup_source, bkup_destination)
+        print(f'{datetime.today().strftime("%H:%M:%S")} - Complete!')
 
-    try:
+    finish_time = time.time() - start_time
 
-        if os.path.isdir(hdd_documents):
-            print("\nExisting Directory Found! Deleting {} ..".format(hdd_documents))
-            os.system('rmdir /S /Q "{}"'.format(hdd_documents))
-            print("{} - Copying {} to {}".format(datetime.datetime.today().strftime("%H:%M:%S"), pc_documents, hdd_documents))
-            shutil.copytree(pc_documents, hdd_documents)
-            print("{} - Complete!".format(datetime.datetime.today().strftime("%H:%M:%S")))
-        else:
-            print("{} - Copying {} to {}".format(datetime.datetime.today().strftime("%H:%M:%S"), pc_documents, hdd_documents))
-            shutil.copytree(pc_documents, hdd_documents)
-            print("{} - Complete!".format(datetime.datetime.today().strftime("%H:%M:%S")))
+    print(f'Section Complete! Duration: {round(finish_time, 2)}')
 
-    except FileExistsError:
-        print("Folder already exists: '{}'".format(hdd_documents))
+    time.sleep(20)
 
-    except PermissionError:
-        print("Access is denied: '{}'".format(hdd_documents))
+# Documents
 
-    # Pictures
+file_bkup(fr'C:\Users\{user}\Desktop\source\pictures', fr'C:\Users\{user}\Desktop\destination\pictures')
 
-    try:
+# Pictures
 
-        if os.path.isdir(hdd_pictures):
-            print("\nDirectory Found! Deleting {} ..".format(hdd_pictures))
-            os.system('rmdir /S /Q "{}"'.format(hdd_pictures))
-            print("{} - Copying {} to {}".format(datetime.datetime.today().strftime("%H:%M:%S"), pc_pictures, hdd_pictures))
-            shutil.copytree(pc_pictures, hdd_pictures)
-            print("{} - Complete!".format(datetime.datetime.today().strftime("%H:%M:%S")))
-        else:
-            print("{} - Copying {} to {}".format(datetime.datetime.today().strftime("%H:%M:%S"), pc_pictures, hdd_pictures))
-            shutil.copytree(pc_pictures, hdd_pictures)
-            print("{} - Complete!".format(datetime.datetime.today().strftime("%H:%M:%S")))
+file_bkup(fr'C:\Users\{user}\Desktop\source\docs', fr'C:\Users\{user}\Desktop\destination\docs')
 
-    except FileExistsError:
-        print("Folder already exists: '{}'".format(hdd_pictures))
+# Software
 
-    except PermissionError:
-        print("Access is denied: '{}'".format(hdd_pictures))
+file_bkup(fr'C:\Users\{user}\Desktop\source\music', fr'C:\Users\{user}\Desktop\destination\music')
 
-    # Music
+# Music
 
-    try:
+file_bkup(fr'C:\Users\{user}\Desktop\source\software', fr'C:\Users\{user}\Desktop\destination\software')
 
-        if os.path.isdir(hdd_music):
-            print("\nDirectory Found! Deleting {} ..".format(hdd_music))
-            os.system('rmdir /S /Q "{}"'.format(hdd_music))
-            print("{} - Copying {} to {}".format(datetime.datetime.today().strftime("%H:%M:%S"), pc_music, hdd_music))
-            shutil.copytree(pc_music, hdd_music)
-            print("{} - Complete!".format(datetime.datetime.today().strftime("%H:%M:%S")))
-        else:
-            print("{} - Copying {} to {}".format(datetime.datetime.today().strftime("%H:%M:%S"), pc_music, hdd_music))
-            shutil.copytree(pc_music, hdd_music)
-            print("{} - Complete!".format(datetime.datetime.today().strftime("%H:%M:%S")))
+# Videos
 
-    except FileExistsError:
-        print("Access is denied: '{}'".format(hdd_music))
+file_bkup(fr'C:\Users\{user}\Desktop\source\videos', fr'C:\Users\{user}\Desktop\destination\videos')
 
-    except PermissionError:
-        print("Access is denied: '{}'".format(hdd_music))
-
-    # Videos
-
-    try:
-
-        if os.path.isdir(hdd_videos):
-            print("\nDirectory Found! Deleting {} ..".format(hdd_videos))
-            os.system('rmdir /S /Q "{}"'.format(hdd_videos))
-            print("{} - Copying {} to {}".format(datetime.datetime.today().strftime("%H:%M:%S"), pc_videos, hdd_videos))
-            shutil.copytree(pc_videos, hdd_videos)
-            print("{} - Complete!".format(datetime.datetime.today().strftime("%H:%M:%S")))
-        else:
-            print("{} - Copying {} to {}".format(datetime.datetime.today().strftime("%H:%M:%S"), pc_videos, hdd_videos))
-            shutil.copytree(pc_videos, hdd_videos)
-            print("{} - Complete!".format(datetime.datetime.today().strftime("%H:%M:%S")))
-
-    except FileExistsError:
-        print("Access is denied: '{}'".format(hdd_videos))
-
-    except PermissionError:
-        print("Access is denied: '{}'".format(hdd_videos))
-
-    # Software
-
-    try:
-
-        if os.path.isdir(hdd_software):
-            print("\nDirectory Found! Deleting {} ..".format(hdd_software))
-            os.system('rmdir /S /Q "{}"'.format(hdd_software))
-            print("{} - Copying {} to {}".format(datetime.datetime.today().strftime("%H:%M:%S"), pc_software, hdd_software))
-            shutil.copytree(pc_software, hdd_software)
-            print("{} - Complete!".format(datetime.datetime.today().strftime("%H:%M:%S")))
-        else:
-            print("{} - Copying {} to {}".format(datetime.datetime.today().strftime("%H:%M:%S"), pc_software, hdd_software))
-            shutil.copytree(pc_software, hdd_software)
-            print("{} - Complete!".format(datetime.datetime.today().strftime("%H:%M:%S")))
-
-    except FileExistsError:
-        print("Access is denied: '{}'".format(hdd_software))
-
-    except PermissionError:
-        print("Access is denied: '{}'".format(hdd_software))
-
-    # Backup Log File
-
-    log = open(r"E:\readme.txt", "a")
-    log.write("\n{} - Backup Complete".format(datetime.datetime.today().strftime("%d/%m/%Y")))
-    log.close()
-
-except FileNotFoundError:
-    print("\nBackup Drive is not connected. Please check connection and try again..")
-
-print("\n{} - Backup Complete!".format(datetime.datetime.today().strftime("%H:%M:%S")))
-input("\nPress any key to exit ..")
+start_time = time.time()
+finish_time = time.time() - start_time
+print('\n---------------')
+print(f'Full Backup Complete! Duration: {round(finish_time, 2)}')
+print()
+input("Press any key to exit ..")
